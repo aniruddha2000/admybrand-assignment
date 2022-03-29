1341a0f2ee936c72df73da1ac21cd597f6b4a2c9
Aniruddha Basak <codewithaniruddha@gmail.com>
2022-03-29 13:19:14 +0530

    Add routes for handling routes for the project
    Signed-off-by: Aniruddha Basak <codewithaniruddha@gmail.com>

---
+++ api/controllers/routes.go
@@ -0,0 +1,19 @@
+package controllers
+
+import "github.com/aniruddha2000/admybrand-assignment/api/middlewares"
+
+func (s *Server) initializeRoutes() {
+
+	// Home Route
+	s.Router.HandleFunc("/", middlewares.SetMiddlewareJSON(s.Home)).Methods("GET")
+
+	// Login Route
+	s.Router.HandleFunc("/login", middlewares.SetMiddlewareJSON(s.Login)).Methods("POST")
+
+	// User Routes
+	s.Router.HandleFunc("/users", middlewares.SetMiddlewareJSON(s.CreateUser)).Methods("POST")
+	s.Router.HandleFunc("/users", middlewares.SetMiddlewareJSON(s.GetUsers)).Methods("GET")
+	s.Router.HandleFunc("/users/{id}", middlewares.SetMiddlewareJSON(s.GetUser)).Methods("GET")
+	s.Router.HandleFunc("/users/{id}", middlewares.SetMiddlewareJSON(middlewares.SetMiddlewareAuthentication(s.UpdateUser))).Methods("PUT")
+	s.Router.HandleFunc("/users/{id}", middlewares.SetMiddlewareAuthentication(s.DeleteUser)).Methods("DELETE")
+}
